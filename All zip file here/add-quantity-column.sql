f39cd7f69c2683dcedccffd322aa926d0665b1ab
galaxy-tanim <saifurjewel785@gmail.com>
2026-07-13 17:43:39 +0600

    Payment method added

--- All zip file here/add-quantity-column.sql
+++
@@ -1,4 +0,0 @@
--- এই একটা লাইন Supabase এর SQL Editor এ Run করো (একবারই লাগবে)
--- এটা "products" টেবিলে সেলারের দেওয়া Quantity সংরক্ষণের জন্য একটা নতুন কলাম যোগ করবে
-
-alter table products add column if not exists requested_stock integer;
